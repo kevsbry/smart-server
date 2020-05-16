fa8b92ed77d9de4ef82bfd791b80f04c84c03718
kevin <nivekbryan09.projects@gmail.com>
2020-05-16 10:49:21 +0800

    Updates

---
+++ precache-manifest.1d5752c734d6e56bd4b670d406c2a887.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad876e7f0dba7bdec621737c8879a1a",
+    "url": "/smart-server/index.html"
+  },
+  {
+    "revision": "39b92ff16435458b026f",
+    "url": "/smart-server/static/css/main.32081871.chunk.css"
+  },
+  {
+    "revision": "6bf04950247f4bd03cfb",
+    "url": "/smart-server/static/js/2.6dfe62ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smart-server/static/js/2.6dfe62ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b92ff16435458b026f",
+    "url": "/smart-server/static/js/main.1086571f.chunk.js"
+  },
+  {
+    "revision": "a3c3e8829df9e641868a",
+    "url": "/smart-server/static/js/runtime-main.bce5f1ea.js"
+  },
+  {
+    "revision": "878d3517ee0f7922564c37d6b46cf4cb",
+    "url": "/smart-server/static/media/app_store.878d3517.svg"
+  },
+  {
+    "revision": "1d768c80cc224915eba403bbbfd7cd52",
+    "url": "/smart-server/static/media/background.1d768c80.svg"
+  },
+  {
+    "revision": "d6fa57bccfd497c2d9a01edc825ac45e",
+    "url": "/smart-server/static/media/background1.d6fa57bc.svg"
+  },
+  {
+    "revision": "d3523ebf7406e1af631cc7b610006c5c",
+    "url": "/smart-server/static/media/background2.d3523ebf.svg"
+  },
+  {
+    "revision": "273e5ba2a7c4ae5ad38684c3641d292c",
+    "url": "/smart-server/static/media/cloud_storage.273e5ba2.svg"
+  },
+  {
+    "revision": "ea5416fdd5ef8e7adbce4a82b4b0f5d6",
+    "url": "/smart-server/static/media/iphone_db.ea5416fd.svg"
+  },
+  {
+    "revision": "4939e8a1629fe26ad7c0c99e4a234a66",
+    "url": "/smart-server/static/media/laptop.4939e8a1.svg"
+  },
+  {
+    "revision": "2f7bac3db724116fb87050decb29988e",
+    "url": "/smart-server/static/media/main_logo.2f7bac3d.svg"
+  },
+  {
+    "revision": "71351f0ccc30cb8b3e7c49807a443806",
+    "url": "/smart-server/static/media/multi_browser.71351f0c.svg"
+  },
+  {
+    "revision": "c09d6763bf4f19cb865353797701f414",
+    "url": "/smart-server/static/media/no_wifi.c09d6763.svg"
+  },
+  {
+    "revision": "7311524b1384c444898142699060c950",
+    "url": "/smart-server/static/media/platform.7311524b.svg"
+  },
+  {
+    "revision": "cd80d8da0a2faae1a8d8d8d269873fde",
+    "url": "/smart-server/static/media/platform_mobile.cd80d8da.svg"
+  },
+  {
+    "revision": "60bb7a15227f83af5957a89a809695af",
+    "url": "/smart-server/static/media/play_store.60bb7a15.svg"
+  }
+]);
